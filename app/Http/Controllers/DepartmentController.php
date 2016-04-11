7d1cace83f14c9c2af4842cf9b9538e74d3b5cb4
trieudh <trieudh@icts.vn>
2016-04-11 11:21:15 +0700

    put routes of Department & Employee under Auth middleware

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -11,7 +11,13 @@ use App\Http\Requests;
 
 class DepartmentController extends Controller
 {
-    //
+    // Put department's routes under Auth middleware
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    // show list of departments
     public function index()
     {
         $departments = Department::all();
--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -8,7 +8,13 @@ use App\Http\Requests;
 
 class EmployeeController extends Controller
 {
-    //
+    // Put employee's routes under Auth middleware
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    // show list of employees
     public function index()
     {
         return view('employee.index');
