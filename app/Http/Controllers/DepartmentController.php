96b94929977cdb532bd7d738814cd88249210ddf
trieudh <trieudh@icts.vn>
2016-04-16 14:50:10 +0700

    create validation for editDepartmentForm

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -83,40 +83,63 @@ class DepartmentController extends Controller
     public function editForm($id)
     {
         $dp = Department::find($id);
-        $dp->manager_name = Employee::find($dp->manager_id)->name;
+        $manager_name = Employee::find($dp->manager_id)->name;
         $employees = $dp->employees;
-        return view('department.editDepartmentForm', compact('dp', 'employees'));
+        return view('department.editDepartmentForm', compact('dp', 'employees', 'manager_name'));
     }
     
     // Edit a department information
     // Need to be enhanced with validation
     public function edit(Request $request)
     {
+        // Customize validation messages
+        $messages = [
+            'dp-name.required' => 'The name field is required.',
+            'dp-office-number.required' => 'The office number field is required.',
+            'dp-manager-id.required' => 'The manager field is required.',
+            'dp-name.string' => 'The name field must be a string.',
+            'dp-office-number.string' => 'The office number field must be a string.',
+            'dp-manager-id.integer' => 'You must select one manager.'
+        ];
+
+        // Validate
+        // If success, continue
+        // Else, flash errors to the session
+        $this->validate($request,[
+            'dp-name' => 'required|string',
+            'dp-office-number' => 'required|string',
+            'dp-manager-id' => 'required|integer'
+        ], $messages);
+        
         $dp_id = $request->input('dp-id');
         $new_dp_name = $request->input('dp-name');
         $new_dp_office_number = $request->input('dp-office-number');
         $new_dp_manager_id = $request->input('dp-manager-id');
-        if ($dp_id && is_numeric($dp_id)) {
-            $dp = Department::find($request->input('dp-id'));
 
-            if ($dp->name === $new_dp_name && $dp->office_number === $new_dp_office_number && $dp->manager_id === $new_dp_manager_id) {
-                $result = 'Department information remains unchanged!';
-                $alert_type = 'warning';
-            }
-            elseif ($new_dp_name != '' && $new_dp_office_number != '' && is_numeric($new_dp_manager_id)) {
-                $dp->name = $new_dp_name;
-                $dp->office_number = $new_dp_office_number;
-                $dp->manager_id = $new_dp_manager_id;
-                $dp->save();
-                $result = 'Department information successfully updated!';
-                $alert_type = 'success';
-            }
-        }
-        else {
-            $result = "Something's wrong. Check your input!";
+        // Find the department with provided id
+        $dp = Department::find($dp_id);
+        // and its employees
+        $employees = $dp->employees;
+        // and name of the manager
+        $manager_name = Employee::find($dp->manager_id)->name;
+
+        // If the provided information from Edit form is not modified, do nothing
+        if ($dp->name === $new_dp_name && $dp->office_number === $new_dp_office_number && $dp->manager_id === $new_dp_manager_id) {
+            $result = 'Department information remains unchanged!';
             $alert_type = 'warning';
         }
-        return view('department.editDepartmentResult', compact('result', 'alert_type', 'dp_id'));
+        else {
+            // Update to database
+            $dp->name = $new_dp_name;
+            $dp->office_number = $new_dp_office_number;
+            $dp->manager_id = $new_dp_manager_id;
+            $dp->save();
+
+            // Create alert message to flash back to session
+            $result = 'Department information successfully updated!';
+            $alert_type = 'success';
+        }
+        return view('department.editDepartmentForm', compact('result', 'alert_type', 'dp', 'employees', 'manager_name'));
     }
 
     // Delete a department
--- resources/views/department/editDepartmentForm.blade.php
+++ resources/views/department/editDepartmentForm.blade.php
@@ -3,6 +3,18 @@
 @section('content')
     <div class="container">
         <div class="row">
+            @if(count($errors) == 0 && isset($result) && isset($alert_type))
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="alert alert-{{$alert_type}} alert-dismissible" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <strong>{{$result}}</strong>
+                    </div>
+                </div>
+            @endif
+        </div>
+        <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">Edit department</div>
@@ -10,35 +22,51 @@
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/department').'/'.$dp->id.'/edit'}}">
                             {!! csrf_field() !!}
 
-                            <div class="form-group">
+                            <div class="form-group{{$errors->has('dp-name') ? ' has-error' : ''}}">
                                 <label class="col-md-4 control-label">Name</label>
 
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="dp-name" value="{{$dp->name}}">
                                     <input type="hidden" class="form-control" name="dp-id" value="{{$dp->id}}">
+                                    @if($errors->has('dp-name'))
+                                        <span class="help-block">
+                                            <strong>{{$errors->first('dp-name')}}</strong>
+                                        </span>
+                                    @endif
                                 </div>
                             </div>
 
-                            <div class="form-group">
+                            <div class="form-group{{$errors->has('dp-office-number') ? ' has-error' : ''}}">
                                 <label class="col-md-4 control-label">Office number</label>
 
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="dp-office-number" value="{{$dp->office_number}}">
+                                    @if($errors->has('dp-office-number'))
+                                        <span class="help-block">
+                                            <strong>{{$errors->first('dp-office-number')}}</strong>
+                                        </span>
+                                    @endif
                                 </div>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group{{$errors->has('dp-manager-id')? ' has-error' : ''}}">
                                 <label class="col-md-4 control-label">Manager</label>
 
                                 <div class="col-md-6">
                                     <select class="form-control" name="dp-manager-id" >
+                                        <option hidden>Select one</option>
                                         @if(sizeof($employees))
                                             @foreach($employees as $em)
                                                 <option value="{{$em->id}}">{{$em->name}}</option>
                                             @endforeach
                                         @else
-                                            <option value="{{$dp->manager_id}}">{{$dp->manager_name}}</option>
+                                            <option value="{{$dp->manager_id}}">{{$manager_name}}</option>
                                         @endif
                                     </select>
+                                    @if($errors->has('dp-manager-id'))
+                                        <span class="help-block">
+                                            <strong>{{$errors->first('dp-manager-id')}}</strong>
+                                        </span>
+                                    @endif
                                 </div>
                             </div>
 
