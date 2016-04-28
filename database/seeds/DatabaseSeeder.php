39a7c3a803eb30ac00060394b5faf73082b9ac10
trieudh <trieudh@icts.vn>
2016-04-28 09:56:09 +0700

    add more seed data into DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -36,7 +36,7 @@ class DatabaseSeeder extends Seeder
         $employee->name = 'Adam M';
         $employee->job_title = 'Manager';
         $employee->email = 'adam_M@gmail.com';
-        $employee->phone_number = '0166443325';
+        $employee->phone_number = '01664433265';
         $employee->department_id = '2';
         $employee->picture = 'uploads/images/icon-user-default.png';
         $employee->save();
@@ -46,7 +46,88 @@ class DatabaseSeeder extends Seeder
         $employee->job_title = 'Manager';
         $employee->department_id = '1';
         $employee->email = 'pxhieu@gmail.com';
-        $employee->phone_number = '09671625378';
+        $employee->phone_number = '0967162578';
+        $employee->picture = 'uploads/images/icon-user-default.png';
+        $employee->save();
+
+        $employee = new App\Employee();
+        $employee->name = 'Example Employee 1';
+        $employee->job_title = 'Assistant';
+        $employee->department_id = '1';
+        $employee->email = 'example_employee_1@gmail.com';
+        $employee->phone_number = '0967162537';
+        $employee->picture = 'uploads/images/icon-user-default.png';
+        $employee->save();
+
+        $employee = new App\Employee();
+        $employee->name = 'Example Employee 2';
+        $employee->job_title = 'Assistant';
+        $employee->department_id = '1';
+        $employee->email = 'example_employee_2@gmail.com';
+        $employee->phone_number = '0967162537';
+        $employee->picture = 'uploads/images/icon-user-default.png';
+        $employee->save();
+
+        $employee = new App\Employee();
+        $employee->name = 'Example Employee 3';
+        $employee->job_title = 'Assistant';
+        $employee->department_id = '1';
+        $employee->email = 'example_employee_3@gmail.com';
+        $employee->phone_number = '0967162537';
+        $employee->picture = 'uploads/images/icon-user-default.png';
+        $employee->save();
+
+        $employee = new App\Employee();
+        $employee->name = 'Example Employee 4';
+        $employee->job_title = 'Assistant';
+        $employee->department_id = '2';
+        $employee->email = 'example_employee_4@gmail.com';
+        $employee->phone_number = '0988888888';
+        $employee->picture = 'uploads/images/icon-user-default.png';
+        $employee->save();
+
+        $employee = new App\Employee();
+        $employee->name = 'Example Employee 5';
+        $employee->job_title = 'Assistant';
+        $employee->department_id = '2';
+        $employee->email = 'example_employee_5@gmail.com';
+        $employee->phone_number = '0988888888';
+        $employee->picture = 'uploads/images/icon-user-default.png';
+        $employee->save();
+
+        $employee = new App\Employee();
+        $employee->name = 'Example Employee 6';
+        $employee->job_title = 'Assistant';
+        $employee->department_id = '2';
+        $employee->email = 'example_employee_6@gmail.com';
+        $employee->phone_number = '0988888888';
+        $employee->picture = 'uploads/images/icon-user-default.png';
+        $employee->save();
+
+        $employee = new App\Employee();
+        $employee->name = 'Example Employee 7';
+        $employee->job_title = 'Assistant';
+        $employee->department_id = '2';
+        $employee->email = 'example_employee_7@gmail.com';
+        $employee->phone_number = '0988888888';
+        $employee->picture = 'uploads/images/icon-user-default.png';
+        $employee->save();
+
+        $employee = new App\Employee();
+        $employee->name = 'Example Employee 8';
+        $employee->job_title = 'Assistant';
+        $employee->department_id = '2';
+        $employee->email = 'example_employee_8@gmail.com';
+        $employee->phone_number = '0988888888';
+        $employee->picture = 'uploads/images/icon-user-default.png';
+        $employee->save();
+
+        $employee = new App\Employee();
+        $employee->name = 'Example Employee 9';
+        $employee->job_title = 'Assistant';
+        $employee->department_id = '2';
+        $employee->email = 'example_employee_9@gmail.com';
+        $employee->phone_number = '0988888888';
         $employee->picture = 'uploads/images/icon-user-default.png';
         $employee->save();
 
