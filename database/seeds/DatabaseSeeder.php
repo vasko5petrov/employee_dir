7f279f1094e5153b5ccde2ba90665627cd20165d
trieudh <trieudh@icts.vn>
2016-04-30 14:14:04 +0700

    2nd update DB seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,115 +23,19 @@ class DatabaseSeeder extends Seeder
         $user->username = 'Admin';
         $user->password = bcrypt('123456');
         $user->save();
-        
-        $employee = new App\Employee();
-        $employee->name = 'Adam';
-        $employee->job_title = 'Developer';
-        $employee->department_id = '1';
-        $employee->email = 'adam@gmail.com';
-        $employee->picture = 'uploads/images/icon-user-default.png';
-        $employee->save();
 
-        $employee = new App\Employee();
-        $employee->name = 'Adam M';
-        $employee->job_title = 'Manager';
-        $employee->email = 'adam_M@gmail.com';
-        $employee->phone_number = '01664433265';
-        $employee->department_id = '2';
-        $employee->picture = 'uploads/images/icon-user-default.png';
-        $employee->save();
+        for ($i = 0; $i < 40; $i++) {
+            $employee = new App\Employee();
+            $employee->name = 'Example Employee '.strval($i + 1);
+            $employee->job_title = 'Assistant';
+            $employee->department_id = $i + 1;
+            $employee->email = 'example_employee'.strval($i + 1).'@gmail.com';
+            $employee->phone_number = '0967162537';
+            $employee->picture = 'uploads/images/icon-user-default.png';
+            $employee->save();
+        }
 
-        $employee = new App\Employee();
-        $employee->name = 'Hieu-PX';
-        $employee->job_title = 'Manager';
-        $employee->department_id = '1';
-        $employee->email = 'pxhieu@gmail.com';
-        $employee->phone_number = '0967162578';
-        $employee->picture = 'uploads/images/icon-user-default.png';
-        $employee->save();
-
-        $employee = new App\Employee();
-        $employee->name = 'Example Employee 1';
-        $employee->job_title = 'Assistant';
-        $employee->department_id = '1';
-        $employee->email = 'example_employee_1@gmail.com';
-        $employee->phone_number = '0967162537';
-        $employee->picture = 'uploads/images/icon-user-default.png';
-        $employee->save();
-
-        $employee = new App\Employee();
-        $employee->name = 'Example Employee 2';
-        $employee->job_title = 'Assistant';
-        $employee->department_id = '1';
-        $employee->email = 'example_employee_2@gmail.com';
-        $employee->phone_number = '0967162537';
-        $employee->picture = 'uploads/images/icon-user-default.png';
-        $employee->save();
-
-        $employee = new App\Employee();
-        $employee->name = 'Example Employee 3';
-        $employee->job_title = 'Assistant';
-        $employee->department_id = '1';
-        $employee->email = 'example_employee_3@gmail.com';
-        $employee->phone_number = '0967162537';
-        $employee->picture = 'uploads/images/icon-user-default.png';
-        $employee->save();
-
-        $employee = new App\Employee();
-        $employee->name = 'Example Employee 4';
-        $employee->job_title = 'Assistant';
-        $employee->department_id = '2';
-        $employee->email = 'example_employee_4@gmail.com';
-        $employee->phone_number = '0988888888';
-        $employee->picture = 'uploads/images/icon-user-default.png';
-        $employee->save();
-
-        $employee = new App\Employee();
-        $employee->name = 'Example Employee 5';
-        $employee->job_title = 'Assistant';
-        $employee->department_id = '2';
-        $employee->email = 'example_employee_5@gmail.com';
-        $employee->phone_number = '0988888888';
-        $employee->picture = 'uploads/images/icon-user-default.png';
-        $employee->save();
-
-        $employee = new App\Employee();
-        $employee->name = 'Example Employee 6';
-        $employee->job_title = 'Assistant';
-        $employee->department_id = '2';
-        $employee->email = 'example_employee_6@gmail.com';
-        $employee->phone_number = '0988888888';
-        $employee->picture = 'uploads/images/icon-user-default.png';
-        $employee->save();
-
-        $employee = new App\Employee();
-        $employee->name = 'Example Employee 7';
-        $employee->job_title = 'Assistant';
-        $employee->department_id = '2';
-        $employee->email = 'example_employee_7@gmail.com';
-        $employee->phone_number = '0988888888';
-        $employee->picture = 'uploads/images/icon-user-default.png';
-        $employee->save();
-
-        $employee = new App\Employee();
-        $employee->name = 'Example Employee 8';
-        $employee->job_title = 'Assistant';
-        $employee->department_id = '2';
-        $employee->email = 'example_employee_8@gmail.com';
-        $employee->phone_number = '0988888888';
-        $employee->picture = 'uploads/images/icon-user-default.png';
-        $employee->save();
-
-        $employee = new App\Employee();
-        $employee->name = 'Example Employee 9';
-        $employee->job_title = 'Assistant';
-        $employee->department_id = '2';
-        $employee->email = 'example_employee_9@gmail.com';
-        $employee->phone_number = '0988888888';
-        $employee->picture = 'uploads/images/icon-user-default.png';
-        $employee->save();
-
-        for ($i =0; $i < 20; $i++) {
+        for ($i =0; $i < 40; $i++) {
             $deparment = new App\Department();
             $deparment->name = 'Example Department '.strval($i + 1);
             $deparment->office_number = '0978561225';
