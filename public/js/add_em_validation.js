45bb6d6d4c7f284159b1bfa845201a09668f8156
trieudh <trieudh@icts.vn>
2016-04-30 13:45:06 +0700

    minor changes on add_em_validation.js

--- public/js/add_em_validation.js
+++ public/js/add_em_validation.js
@@ -1,14 +1,17 @@
-$(document).ready(function (){
+$(document).ready(function () {
     var $input = $('#em-name, #em-job-title'),
         $em_button = $('#em_button');
-    var trigger = false;
-    $input.each(function() {
-        if ($(this).val() == '') {
-            trigger = true;
-        }
+    $em_button.prop('disabled', true);
+    $input.keyup(function () {
+        var trigger = false;
+        $input.each(function() {
+            if ($(this).val() == '') {
+                trigger = true;
+            }
+        });
         trigger ? $em_button.prop('disabled', true) : $em_button.prop('disabled', false);
-
     });
+
     $input.change(function() {
         var trigger = false;
         $input.each(function() {
