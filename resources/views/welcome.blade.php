b547d499cb7cc4a8ba7d388174df91267e288119
trieudh <trieudh@icts.vn>
2016-05-26 17:54:48 +0700

    remove animation from EM/DP/Welcome view: fix delete modal bug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container animated zoomIn" xmlns="http://www.w3.org/1999/html">
+<div class="container" xmlns="http://www.w3.org/1999/html">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
