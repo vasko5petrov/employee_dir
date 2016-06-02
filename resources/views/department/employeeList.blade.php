cbf2b70fb5e4aa3244a720c16a5e9d52cbc741cc
dang trieu <dangtrieu25@gmail.com>
2016-06-03 01:14:39 +0700

    rebuild view: department employee list

--- resources/views/department/employeeList.blade.php
+++ resources/views/department/employeeList.blade.php
@@ -1,49 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container animated zoomIn">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <a href="{{url('/department').'/'.$dp->id.'/detail'}}">{{$dp->name}} </a>
-                        <i class="fa fa-chevron-right" aria-hidden="true"></i> Employees ({{sizeof($employees)}})
-                        <div class="pull-right">
-                            <a href="{{URL::previous()}}" class="btn btn-primary btn-xs">
-                                <i class="fa fa-btn fa-chevron-left" aria-hidden="true"></i>Back
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="panel-body">
-                        <table class="table table-hover">
-                            <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Name</th>
-                                <th>Job Title</th>
-                                <th>Email</th>
-                                <th>Phone Number</th>
-                            </tr>
-                            <tbody>
-                            @foreach($employees as $index=>$em)
-                                <tr>
-                                    <td>{{($employees->currentPage()-1)*10+$index+1}}</td>
-                                    <td><a href="{{url('/employee').'/'.$em->id.'/detail'}}">{{$em->name}}</a></td>
-                                    <td>{{$em->job_title}}</td>
-                                    <td>{{$em->email}}</td>
-                                    <td>{{$em->phone_number}}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                            </thead>
-                        </table>
-                        <center>
-                            {!! $employees->render() !!}
-                        </center>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="container">
+        <h4>
+            <a href="{{url('/department').'/'.$dp->id.'/detail'}}">{{$dp->name}}</a>
+            <span class="chip right">{{ sizeof($employees) }} {{ sizeof($employees) != 1 ? 'employees' : 'employee' }}</span>
+        </h4>
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Job Title</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+            </tr>
+            <tbody>
+            @foreach($employees as $index=>$em)
+                <tr>
+                    <td>{{($employees->currentPage()-1)*10+$index+1}}</td>
+                    <td><a href="{{url('/employee').'/'.$em->id.'/detail'}}">{{$em->name}}</a></td>
+                    <td>{{$em->job_title}}</td>
+                    <td>{{$em->email}}</td>
+                    <td>{{$em->phone_number}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+            </thead>
+        </table>
+        <center>
+            {!! $employees->render() !!}
+        </center>
     </div>
 @endsection
